9a4e3a94cd5e15e142a25aba92c02d99532ef21a
KyotoBai <kyotobai.lyc@gmail.com>
2023-11-09 23:02:09 -0500

    Basic Grid
    1. Add grid and cursor
    2. Add grid shader

--- Assets/Scripts/PlacementSys.cs
+++ Assets/Scripts/PlacementSys.cs
@@ -7,11 +7,16 @@ public class PlacementSys : MonoBehaviour
     [SerializeField]
     private InputManager inputManager;
     [SerializeField]
-    private GameObject mouseIndicator;
+    private GameObject mouseIndicator, cellIndicator;
+    [SerializeField]
+    private Grid grid;
+
 
     private void Update()
     {
         Vector3 mousePos = inputManager.GetSelectedMapPosition();
+        Vector3Int gridPos = grid.WorldToCell(mousePos);
         mouseIndicator.transform.position = mousePos;
+        cellIndicator.transform.position = grid.CellToWorld(gridPos);
     }
 }
