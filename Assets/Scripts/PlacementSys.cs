43239553215c477510b55d993b12386be6433e8c
Dingwen Wang <wangdingwen32@gmail.com>
2023-11-10 11:32:35 -0500

    Leveling and prevent overlap placement

--- Assets/Scripts/PlacementSys.cs
+++ Assets/Scripts/PlacementSys.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Rendering;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
@@ -21,9 +22,16 @@ public class PlacementSys : MonoBehaviour
 
     [SerializeField]
     public string rotationKeyPress;
+    [SerializeField]
+    public string levelUpKeyPress;
+    [SerializeField]
+    public string levelDownKeyPress;
 
     private GameObject placementHint;
     private GameObject placementHintParent;
+
+    private Vector3 levelStep;
+    private int currentLevel = 0;
 
     public void StartPlacement(int ID)
     {
@@ -51,6 +59,8 @@ public class PlacementSys : MonoBehaviour
     private void Start()
     {
         StopPlacement();
+        Vector3 cellSize = grid.cellSize;
+        levelStep = new Vector3(0, cellSize.x, 0);
     }
 
     private void StopPlacement()
@@ -64,6 +74,7 @@ public class PlacementSys : MonoBehaviour
         placementHint = null;
         if (placementHintParent != null) Destroy(placementHintParent);
         placementHintParent = null;
+        currentLevel = 0;
     }
 
     private void PlaceStructure()
@@ -74,9 +85,22 @@ public class PlacementSys : MonoBehaviour
         }
         Vector3 mousePos = inputManager.GetSelectedMapPosition();
         Vector3Int gridPos = grid.WorldToCell(mousePos);
+        Vector3Int cellPosition = grid.WorldToCell(mousePos);
+        Vector3 cellCenterInWorld = grid.CellToWorld(cellPosition) + (grid.cellSize * 0.5f) + new Vector3(0, currentLevel * grid.cellSize.y, 0);
+        Vector3 colliderCenter = grid.cellSize * 0.5f;
+        bool isCellOccupied = Physics.CheckBox(cellCenterInWorld, colliderCenter, Quaternion.identity, LayerMask.GetMask("PlacementObject"));
+        if (isCellOccupied)
+        {
+            return;
+        }
+
         GameObject newGameObj = Instantiate(database.objData[selectObjIndex].Prefab);
         newGameObj.transform.position = placementHint.transform.position;
         newGameObj.transform.rotation = placementHint.transform.rotation;
+        newGameObj.layer = LayerMask.NameToLayer("PlacementObject");
+        BoxCollider newObjectCollider = newGameObj.GetComponent<BoxCollider>();
+        newObjectCollider.center = new Vector3(grid.cellSize.x * 0.5f, grid.cellSize.y * 0.5f, grid.cellSize.z * 0.5f);
+        newObjectCollider.size = 0.9f * grid.cellSize;
     }
 
     private void Update()
@@ -89,11 +113,22 @@ public class PlacementSys : MonoBehaviour
         Vector3 mousePos = inputManager.GetSelectedMapPosition();
         Vector3Int gridPos = grid.WorldToCell(mousePos);
         mouseIndicator.transform.position = mousePos;
-        cellIndicator.transform.position = grid.CellToWorld(gridPos);
-        placementHintParent.transform.position = grid.CellToWorld(gridPos);
+        cellIndicator.transform.position = grid.CellToWorld(gridPos) + currentLevel * levelStep;
+        placementHintParent.transform.position = grid.CellToWorld(gridPos) + currentLevel * levelStep;
         if (Input.GetKeyDown(rotationKeyPress))
         {
             RotateHint(grid.GetCellCenterWorld(gridPos), 90);
+        }
+        if (Input.GetKeyDown(levelUpKeyPress))
+        {
+            currentLevel += 1;
+        }
+        if (Input.GetKeyDown(levelDownKeyPress))
+        {
+            if (currentLevel > 0)
+            {
+                currentLevel -= 1;
+            }
         }
     }
 }
