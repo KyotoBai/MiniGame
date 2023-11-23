d50bbb96306bbc0a0410490c917653df6cf2bb46
KyotoBai <kyotobai.lyc@gmail.com>
2023-11-23 12:37:55 -0500

    Change UI logic
    change UI and add turret

--- Assets/Scripts/UI framework/Panel and Sub Panel/OpenClose.cs
+++ Assets/Scripts/UI framework/Panel and Sub Panel/OpenClose.cs
@@ -1,47 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
+using static UnityEngine.Rendering.DebugUI;
 
 public class OpenClose : MonoBehaviour
 {
-    public GameObject subPanel;
-    private uint i = 0;
+    public GameObject parentPanel;
+    private GameObject[] SubPanels;
+    private bool[] panelStatus;
 
     public void Start()
     {
-        subPanel.SetActive(false);
+        SubPanels = new GameObject[parentPanel.transform.childCount];
+        panelStatus = new bool[parentPanel.transform.childCount];
+
+        for (int i = 0; i < parentPanel.transform.childCount; i++)
+        {
+            SubPanels[i] = parentPanel.transform.GetChild(i).gameObject;
+            panelStatus[i] = false;
+            SubPanels[i].SetActive(false);
+        } 
     }
 
     public void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            subPanel.SetActive(false);
-            i = 0;
+            foreach (GameObject panel in SubPanels)
+            {
+                panel.SetActive(false);
+            }
         }
     }
 
-    public void OnButtomClick()
+    public void OnButtomClick(int i)
     {
-        i++;
-        if (i < 2)
+        // Debug.Log("Panel " + i + " curr: " + panelStatus[i]);
+        // close panel if curr panel status is ture
+        if (panelStatus[i])
         {
+            SubPanels[i].SetActive(false);
+            foreach (GameObject panel in SubPanels)
+            {
+                panel.SetActive(false);
+            }
+            for (int j = 0; j < panelStatus.Length; j++)
+            {
+                panelStatus[j] = false;
+            }
         }
         else
         {
-            i = 0;
+            SubPanels[i].SetActive(true);
+            panelStatus[i] = true;
+            
+            for (int j = 0; j < panelStatus.Length; j++)
+            {
+                if (j != i)
+                {
+                    panelStatus[j] = false;
+                    SubPanels[j].SetActive(false);
+                }
+            }
         }
-
-        if (i == 1)
-        {
-            subPanel.SetActive(true);
-
-        }
-        else
-        {
-            subPanel.SetActive(false);
-        }
-
     }
-
 }
