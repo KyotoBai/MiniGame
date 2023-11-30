8647af42b97ea23c77907759ba5f522bb70b7d6a
KyotoBai <kyotobai.lyc@gmail.com>
2023-11-30 18:53:21 -0500

    Tree prefab

--- Assets/Scripts/Enemy AI/EnemyMove.cs
+++ Assets/Scripts/Enemy AI/EnemyMove.cs
@@ -17,5 +17,9 @@ public class EnemyMove : MonoBehaviour
     void Update()
     {
         this.Agent.SetDestination(this.target.position);
+        if (Agent.path.status == NavMeshPathStatus.PathPartial)
+        {
+            Debug.Log("Partial path found");
+        }
     }
 }
