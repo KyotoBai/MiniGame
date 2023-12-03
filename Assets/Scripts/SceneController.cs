54735a906efa439d3ea4864759de95572a424257
Dingwen Wang <wangdingwen32@gmail.com>
2023-12-03 01:41:46 -0500

    Add SceneController

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("Mainmenu");
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
