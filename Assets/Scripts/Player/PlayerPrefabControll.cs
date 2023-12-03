91fc89dc1214e2d74906dfc6f755e9d3e0fdfe77
KyotoBai <kyotobai.lyc@gmail.com>
2023-12-02 19:51:41 -0500

    Add a gun

--- Assets/Scripts/Player/PlayerPrefabControll.cs
+++ Assets/Scripts/Player/PlayerPrefabControll.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerPrefabControll : MonoBehaviour
 {
@@ -24,6 +25,16 @@ public class PlayerPrefabControll : MonoBehaviour
         {
             transform.GetChild(0).gameObject.SetActive(false);
             transform.GetChild(1).gameObject.SetActive(true);
+
+            if (transform.GetChild(1).GetChild(0).childCount == 0)
+            {
+                GameObject selectedGunPrefab = gunPrefabs[0];
+
+                // Instantiate the tree
+                GameObject newGun = Instantiate(selectedGunPrefab, transform.GetChild(1).GetChild(0).position, Quaternion.Euler(0, 0, 0));
+
+                newGun.transform.SetParent(transform.GetChild(1).GetChild(0), true);
+            }
         }
         else
         {
